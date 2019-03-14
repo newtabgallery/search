f0b2b444a04aaa04563d0a9075eec49768c72633
williams4027 <williams.4027@gmail.com>
2019-03-14 19:56:48 -0400

    Add resutls to the ADM search.

--- _template/index.php
+++ _template/index.php
@@ -15,7 +15,8 @@
     'v' => '1.2',
     'ip' => get_client_ip_server(),
     'ua' => $_SERVER['HTTP_USER_AGENT'],
-    'rfr' => $_SERVER['HTTP_REFERER']
+    'rfr' => $_SERVER['HTTP_REFERER'],
+    'results' => 20
   );
   $ad_marketplace_response = file_get_contents('https://' . $ad_marketplace_partner . '.tiles.ampfeed.com/tiles?' . http_build_query($ad_marketplace_params));
   $ad_marketplace_json = json_decode($ad_marketplace_response, true);

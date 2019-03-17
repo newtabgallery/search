9013638ad073c0cf5515a5822bb8330e69f30142
williams4027 <williams.4027@gmail.com>
2019-03-17 10:30:26 -0400

    Add GA script.

--- _template/index.php
+++ _template/index.php
@@ -44,6 +44,19 @@
 
   <!-- Custom styles for this template -->
   <link href="../_assets/css/landing-page.min.css?v=<?php echo filemtime('../_assets/css/landing-page.min.css'); ?>" rel="stylesheet">
+
+  <script type="text/javascript">
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-127110494-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+  </script>
 </head>
 
 <?php echo $background_image_style; ?>

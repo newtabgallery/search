f071b972239d179523fd40a85c140ed8ff6ece72
williams4027 <williams.4027@gmail.com>
2019-03-25 21:15:22 -0400

    Added omnibus-search.php endpoint.

---
+++ omnibus-search.php
@@ -0,0 +1,23 @@
+<?php
+  require_once($_SERVER["DOCUMENT_ROOT"]."/search-utils.php");
+
+  if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+    header('Access-Control-Allow-Headers: token, Content-Type');
+    header('Access-Control-Max-Age: 1728000');
+    header('Content-Length: 0');
+    header('Content-Type: text/plain');
+    die();
+  }
+
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+
+  $qt = '';
+  if (isset($_GET["q"]) && !empty($_GET["q"])) {
+    $qt = $_GET["q"];
+  }
+
+  header("Location: https://www.my-search.com/search?q=$qt&zoneid=89128928");

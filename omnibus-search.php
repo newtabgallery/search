e3beaac4860443595e81c05108d83374c19c4760
williams4027 <williams.4027@gmail.com>
2019-03-30 12:44:57 -0400

    Add GA server side tracking.

--- omnibus-search.php
+++ omnibus-search.php
@@ -9,6 +9,8 @@
     die();
   }
 
+  require_once($_SERVER["DOCUMENT_ROOT"]."/search-utils.php");
+
   header('Access-Control-Allow-Origin: *');
   header('Content-Type: application/json');
   header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
@@ -16,6 +18,29 @@
   $qt = '';
   if (isset($_GET["q"]) && !empty($_GET["q"])) {
     $qt = $_GET["q"];
+  }
+  
+  try {
+    # Start GA request
+    $ga_params = array(
+        'v' => 1,
+        'tid' => 'UA-127110494-1',
+        'cid' => uniqid(),
+        't' => 'pageview',
+        'dh' => 'search.newtabgallery.com',
+        'dp' => urlencode(utf8_encode("/omnibus-search.php?q=$qt")),
+        'dt' => urlencode(utf8_encode("omnibus-search"))
+    );
+    $payload = http_build_query($ga_params);
+
+    $ga_curl = curl_init();
+    curl_setopt($ga_curl, CURLOPT_URL, 'https://www.google-analytics.com/collect');
+    curl_setopt($ga_curl, CURLOPT_POSTFIELDS, $payload);
+    curl_setopt($ga_curl, CURLOPT_RETURNTRANSFER, 1);
+    $ga_response = curl_exec($ga_curl);
+    curl_close($ga_curl);
+  } catch (Exception $e) {
+    error_log($e);
   }
 
   header("Location: https://www.my-search.com/search?q=$qt&zoneid=89128928");

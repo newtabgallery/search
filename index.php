86e28ad67e38e7fd496d2f24e8ff5effc8c01044
Michael Kaply <345868+mkaply@users.noreply.github.com>
2019-09-05 12:21:31 -0500

    Default search

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$title = "New Tab Gallery";
+
+// NewTabGallery: Edit these to change the rendered background images
+$background_image_count = 0;
+$background_image_style = "";
+
+include('_template/index.php');
+?>

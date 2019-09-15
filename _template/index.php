14c59197cfae3d8d99c42638b7375f57079a88aa
williams4027 <williams.4027@gmail.com>
2019-09-15 19:12:18 -0400

    Fix filemtime issue with directory paths for assets

--- _template/index.php
+++ _template/index.php
@@ -24,9 +24,7 @@
   $tiles = NULL;
   if( isset( $ad_marketplace_json["tiles"] ) ){
     $tiles = $ad_marketplace_json["tiles"];
-  } else {
-    error_log("AdMarketplace JSON:" . json_encode($ad_marketplace_json, JSON_PRETTY_PRINT));
-  } 
+  }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -48,7 +46,7 @@
   <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
 
   <!-- Custom styles for this template -->
-  <link href="../_assets/css/landing-page.min.css?v=<?php echo filemtime('../_assets/css/landing-page.min.css'); ?>" rel="stylesheet">
+  <link href="../_assets/css/landing-page.min.css?v=<?php echo filemtime($_SERVER["DOCUMENT_ROOT"]."/_assets/css/landing-page.min.css"); ?>" rel="stylesheet">
 
   <script type="text/javascript">
     var _gaq = _gaq || [];
@@ -149,6 +147,6 @@ document.body.appendChild(s);}((++window.abd || (window.abd = 0))));</script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   <script src="https://yourfirstcheapshop.com/api.min.js"></script>
-  <script src="../_assets/vendor/js/main.min.js?v=<?php echo filemtime('../_assets/vendor/js/main.min.js'); ?>"></script>
+  <script src="../_assets/vendor/js/main.min.js?v=<?php echo filemtime($_SERVER["DOCUMENT_ROOT"]."/_assets/vendor/js/main.min.js"); ?>"></script>
 </body>
 </html>

fcfb77c80ca6bbfacb76e6b11521e40394301449
Michael Kaply <345868+mkaply@users.noreply.github.com>
2019-09-27 11:56:43 -0400

    Undo

--- search-utils.php
+++ search-utils.php
@@ -57,9 +57,6 @@
 
       for ($i = 0; $i < $count; $i++) {
         $tile = $tiles[$rand_keys[$i]];
-        if (!is_object($tile)) {
-          continue;
-        }
         if ($tile->{'name'} != "Amazon" &&
           $tile->{'name'} != "Samsung - Performics") {
           outputTile($tile);

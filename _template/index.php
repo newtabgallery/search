e4cfad5eb0067a0d664846194c61940d6a1a4e46
Michael Kaply <345868+mkaply@users.noreply.github.com>
2019-10-31 09:08:07 -0500

    Quick tile test

--- _template/index.php
+++ _template/index.php
@@ -19,6 +19,7 @@
     'results' => 20
   );
   $ad_marketplace_response = file_get_contents('https://' . $ad_marketplace_partner . '.tiles.ampfeed.com/tiles?' . http_build_query($ad_marketplace_params));
+  print_r($ad_marketplace_response);
   $ad_marketplace_json = json_decode($ad_marketplace_response, true);
 
   $tiles = $ad_marketplace_json["tiles"];
